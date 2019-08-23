e677b2f7644eba1f6a9212f89eb5b7c7b184d63f
Piyush Kumar <piyushsahsah.sah@gmail.com>
2019-08-23 13:01:54 +0530

    Update

--- 23-08-2019/cameraman_subplot.m
+++ 23-08-2019/cameraman_subplot.m
@@ -1,8 +1,8 @@
 I = imread('cameraman.tif');
 a = 1;
-for i=1:32:256
-    for j=1:32:256
-       subplot(2,4,a);
-       imshow(I(i:i+31,j:j+31));
+for i=1:256/2:256
+    for j=1:256/4:256
+       subplot(2,4,a), imshow(I(i:i+256/2,j:j+256/4));
+       a = a+1;
     end
 end
